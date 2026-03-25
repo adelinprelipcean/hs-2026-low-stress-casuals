7bf4a46f4cb066f3d15143323cadc5c216a1d169
georgeboca1 <gboca4@gmail.com>
2026-03-25 21:48:08 +0200

    added battery card, bug fixes

--- mobile/app/src/main/java/com/example/esp32_diagtool/api/EspService.kt
+++
@@ -1,9 +0,0 @@
-package com.example.esp32_diagtool.api
-
-import com.example.esp32_diagtool.model.EspData
-import retrofit2.http.GET
-
-interface EspService {
-    @GET("get_info")
-    suspend fun getInfo(): EspData
-}
