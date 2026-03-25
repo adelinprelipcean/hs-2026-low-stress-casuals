9d0cb9c7bb38a58629403b379832d6bd830a6b09
Alexandru Rusu <62150899+alexandrusu1@users.noreply.github.com>
2026-03-25 19:57:33 +0200

    Fix RTC persistent time and move secrets.h to include directory

---
+++ firmware/hs_hardware_backend/include/secrets.h
@@ -0,0 +1,10 @@
+// !! ACEST FISIER CONTINE DATE SENSIBILE !!
+// !! NU IL URCA PE GITHUB - ESTE IN .gitignore !!
+// Copiaza secrets.h.example -> secrets.h si completeaza cu credentialele tale.
+
+#pragma once
+
+#define MQTT_SERVER "13c35f0a5bde4564be3ea561c26c7c3b.s1.eu.hivemq.cloud"
+#define MQTT_PORT   8883
+#define MQTT_USER   "esp32mqtt"
+#define MQTT_PASS   "L7sqBG*9+w2m"
