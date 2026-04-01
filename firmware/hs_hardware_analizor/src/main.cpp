8270381c7a08114ca26c1c5b692ece068c13c581
Adelin Prelipcean <97390311+adelinprelipcean@users.noreply.github.com>
2026-04-01 18:49:32 +0300

    "Analyzer update"

--- firmware/hs_hardware_analizor/src/main.cpp
+++ firmware/hs_hardware_analizor/src/main.cpp
@@ -1,17 +1,14 @@
+#include "esp_log.h"
 #include "logic_analyzer_sump.h"
 #include <Arduino.h>
-#include "esp_log.h"
 
-// Declararea externa a generatorului de semnale de test (din logic_analyzer_test_sample.c)
+// Declararea externa a generatorului de semnale de test (din
+// logic_analyzer_test_sample.c)
 extern "C" void test_sample_init();
 
 void setup() {
   // Dezactivam complet logurile seriale pentru a nu corupe protocolul SUMP
   esp_log_level_set("*", ESP_LOG_NONE);
-
-  // Initializam generatorul de semnale de test
-  // Acesta va genera semnale pe diversi pini (4, 5, 6, 7) pentru a verifica analizorul
-  test_sample_init();
 
   // Initializam analizorul pentru PulseView/SUMP
   // Acesta va folosi USB-Serial-JTAG (pinii interni 18/19 pe Super Mini)
